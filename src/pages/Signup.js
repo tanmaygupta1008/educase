52e5a1f516f3c0e75111a36e0403c0c061f6382e
Tanmay Gupta <guptatanmay1008@gmail.com>
2025-08-22 00:26:10 +0530

    signup page and its styling

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,35 +1,65 @@
 
 import Icon from './Icon';
+import { useNavigate } from 'react-router-dom';
 
 // function Signup({ onNavigate }) {
 function Signup() {
+
+  const navigate = useNavigate();
+
+  const navprofile = () => {
+    navigate("/profile")
+  }
+
+
     return (
-        <div>
+        <div className="popx-create-account-page popx-page-container">
             <h1>Create your PopX account</h1>
             <div className='popx-input-group'>
               <label className='popx-input-label'>Full Name*</label>
-              <input type="text" placeholder="Marry Doe" required className="popx-input" />
+              <input type="text" placeholder="Marry Doe" className="popx-input" required/>
             </div>
 
             <div className='popx-input-group'>
               <label className='popx-input-label'>Phone Number*</label>
-              <input type="tel" placeholder="Marry Doe" required className="popx-input" />
+              <input type="tel" placeholder="Marry Doe" className="popx-input" required/>
             </div>
 
             <div className='popx-input-group'>
               <label className='popx-input-label'>Email Address*</label>
-              <input type="email" placeholder="Marry Doe" required className="popx-input" />
+              <input type="email" placeholder="Marry Doe" className="popx-input" required/>
             </div>
 
             <div className='popx-input-group'>
               <label className='popx-input-label'>Password*</label>
-              <input type="password" placeholder="Marry Doe" required className="popx-input" />
+              <input type="password" placeholder="Marry Doe" className="popx-input" required/>
             </div>
 
             <div className='popx-input-group'>
               <label className='popx-input-label'>Company Name*</label>
-              <input type="text" placeholder="Marry Doe" required className="popx-input" />
+              <input type="text" placeholder="Marry Doe" className="popx-input" required/>
             </div>
+
+            <div className="popx-radio-group">
+              <span className="block text-gray-500 mb-2">Are you an Agency?*</span>
+              <div className="flex items-center space-x-4 options">
+                <label className="popx-radio-label">
+                  <input type="radio" name="agency" className="popx-radio-input" defaultChecked />
+                  <span className="ml-2 text-gray-700">Yes</span>
+                </label>
+                <label className="popx-radio-label">
+                  <input type="radio" name="agency" className="popx-radio-input" />
+                  <span className="ml-2 text-gray-700">No</span>
+                </label>
+              </div>
+            </div>
+
+            <button
+              onClick={navprofile}
+              className="popx-button popx-button-primary"
+            >
+              Create Account
+            </button>
             
         </div>
         
